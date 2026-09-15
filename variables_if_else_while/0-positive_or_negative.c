8e3e287d08a392b925cfea9713af133f91f54691
Ikenna Onugha <i.onugha@alustudent.com>
2026-09-15 22:28:29 +0100

    Fix printf statements for number classification

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,10 +11,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		printf("%d is negative\n");
+		printf("%d is negative\n" , n);
 	else if (n < 0)
-		printf("%d is positive\n");
+		printf("%d is positive\n" , n);
 	else
-		print("%d is zero\n");
+		print("%d is zero\n" , n);
 	return (0);
 }
